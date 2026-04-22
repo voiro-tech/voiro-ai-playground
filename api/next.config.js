b1a64554c67454ec38be033ccfbf35d44a26db23
anilkarat <developer@voiro.com>
2026-04-22 14:13:36 +0530

    Update next.config.js

--- api/next.config.js
+++ api/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, x-api-key" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
         ],
       },
     ];
